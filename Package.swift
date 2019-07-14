fe319c592b3619cea62eec1b626c616adcca5e90
Trond Kjeldås <trond@kjeldas.no>
2019-07-14 22:12:54 +0200

    Updated Package.swift to build with Swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
@@ -9,7 +9,8 @@ let package = Package(
     targets: [
         .target(
             name: "CocoaAsyncSocket",
-            path: "Source/GCD"
+            path: "Source/GCD",
+            publicHeadersPath: "Source"
         )
     ]
 )
